c76e09f71db8160534742eb27d558bb5d63c44de
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-06-06 23:00:25 -0300

    chore: criado e estlizado componente header

--- frontend/typescript/mesa_de_trabalho05/src/components/header/index.tsx
+++ frontend/typescript/mesa_de_trabalho05/src/components/header/index.tsx
@@ -1,13 +1,13 @@
 export function Header(){
   return(
-    <header>
-      <h1>DHProducts</h1>
-      <div>
-        <ul>
+    <header className="flex justify-between items-center py-5 px-12 bg-primaryColor ">
+      <h1 className="flex text-txtBlackColor text-xl font-bold">DHProducts</h1>
+      <div className="flex items-center gap-11 ">
+        <ul className="flex gap-11 text-txtBlackColor text-xl cursor-pointer">
           <li>Produtos</li>
           <li>Quem somos</li>
         </ul>
-        <button>Sair</button>
+        <button className="text-txtBlackColor text-xl cursor-pointer hover:rounded-xl hover:bg-white duration-700 px-6 py-1">Sair</button>
       </div>
     </header>
   )
